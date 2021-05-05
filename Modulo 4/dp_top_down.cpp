207770fe62d9899b82339a3532981b0e9a800b89
Rodolfo Christian Catunta Uturunco <rodolfo.catunta.uturunco@gmail.com>
2021-05-05 19:42:08 -0400

    DP Top Down

---
+++ Modulo 4/dp_top_down.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define MINF -1000000000
+
+int M,C;
+vector<vector<int> >lista;
+int cont_recursion=0;
+// Tabla de Memoizar
+int memo[210][25];
+int shop(int money, int g){ // Comprar el item g con la contidad de dinero money
+    int res=MINF;
+    // Casos Base
+    if(money<0) return MINF; // la ruta no genera una solucion valida
+    if(memo[money][g] != -1) return memo[money][g];  // Ver si ya tenemos la respuesta
+    if(g==C) return memo[money][g] = M-money;// Acabamos de comprar todo 
+    // Recursion
+    for(int i=0;i<lista[g].size();i++){
+        int precio = lista[g][i];
+        res = max(res,shop(money-precio,g+1)); // Comprando el item g[i]
+    }
+    return memo[money][g] = res;
+}
+
+int main(){
+    int cp,precio_modelo,K;
+    cin>>cp;
+    while(cp--){
+        cont_recursion=0;
+        // Reseteamos la tabla memo
+        memset(memo,-1,sizeof memo);
+        cin>>M>>C;
+        lista.assign(C,vector<int>());
+        for(int i=0;i<C;i++){
+            cin>>K;
+            for(int j=0;j<K;j++){
+                cin>>precio_modelo;
+                lista[i].push_back(precio_modelo);
+            }
+        }
+        cout<<shop(M,0)<<endl;
+        cout<<"Cont recursion "<<cont_recursion<<endl;
+    }
+    
+    return 0;
+}
